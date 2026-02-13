278623e65484ada8c6f75dc59a9154fbc1aca232
Richard Berg <ars@richardberg.net>
2026-02-13 04:50:30 -0500

    fix CG queries broken by new invite filter

--- choirgenius.py
+++ choirgenius.py
@@ -102,6 +102,7 @@ class ChoirGenius:
 
         data = {
             "sets[]": "g4account::role::member",
+            "show_all_events": "1",
             "event_type[]": event_type.value,
             "range_start[date]": date_from.strftime(DATE_FORMAT),
             "range_end[date]": date_to_exclusive.strftime(DATE_FORMAT),
