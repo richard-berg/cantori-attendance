dab102918c7638845f8f7e97bd996f15253c0c0b
Richard Berg <ars@richardberg.net>
2026-05-13 23:22:56 -0400

    ChoirGenius is back to using closed date ranges again

--- choirgenius.py
+++ choirgenius.py
@@ -97,15 +97,12 @@ class ChoirGenius:
         hidden_inputs = soup.select(f'#{css_id} input[type="hidden"]')
         hidden_fields = {str(i.attrs["name"]): str(i.attrs["value"]) for i in hidden_inputs}
 
-        # CG seems to have switched to half-open date ranges
-        date_to_exclusive = date_to + timedelta(days=1)
-
         data = {
             "sets[]": "g4account::role::member",
             "show_all_events": "1",
             "event_type[]": event_type.value,
             "range_start[date]": date_from.strftime(DATE_FORMAT),
-            "range_end[date]": date_to_exclusive.strftime(DATE_FORMAT),
+            "range_end[date]": date_to.strftime(DATE_FORMAT),
             "export": "Export",
         }
         data.update(hidden_fields)
